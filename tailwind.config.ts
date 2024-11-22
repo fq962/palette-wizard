c3ad008b64ce85a7ded34db0e1900c05ef3b723e
haide <df.quintanillah@gmail.com>
2024-11-22 09:45:20 -0600

    add FlyonUI integration in Tailwind CSS configuration and update dependencies

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
+    "./node_modules/flyonui/dist/js/*.js",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,5 +15,8 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    (await import("flyonui")).default,
+    // (await import("flyonui/plugin")).default, // Require only if you want to use FlyonUI JS component
+  ],
 } satisfies Config;
