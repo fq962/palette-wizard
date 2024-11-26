b51f3ae6f01ff533ca700568bdbac98999c958fb
haide <df.quintanillah@gmail.com>
2024-11-26 15:16:26 -0600

    Add seasonal snow effect for November with background gradient

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,9 +2,19 @@ import FlyonuiScript from "@/components/FlyonuiScript";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  const mesActual = new Date().getMonth() + 1;
   return (
     <Html lang="en" className="bg-transparent" data-theme="light">
-      <Head />
+      <Head>
+        {mesActual === 11 && (
+          <>
+            <script defer src="https://app.embed.im/snow.js"></script>
+            <style>
+              {`body {background: radial-gradient(circle, rgba(55, 132, 183, 0.7), rgba(9, 29, 56, 0.9));`}
+            </style>
+          </>
+        )}
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
