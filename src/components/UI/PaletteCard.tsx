3aa0b24715145fc5b8283524150ed8f1222a6228
Milton Barrientos <miltonbarrientos2@gmail.com>
2024-11-27 14:05:36 -0600

    add animation effects to PaletteCard component using framer motion

--- src/components/UI/PaletteCard.tsx
+++ src/components/UI/PaletteCard.tsx
@@ -1,3 +1,4 @@
+import { motion } from "motion/react";
 import { useState } from "react";
 
 interface PaletteCardProps {
@@ -22,39 +23,51 @@ export const PaletteCard = ({ color, name, textColor }: PaletteCardProps) => {
   };
 
   return (
-    <div
-      className="card sm:max-w-44 h-52 group relative overflow-hidden shadow-flat"
-      style={{ backgroundColor: color }}
+    <motion.div
+      whileHover={{ scale: 1.05 }}
+      // Animate when this value changes:
+      animate={{ scale: 1 }}
+      // Fade in when the element enters the viewport:
+      whileInView={{ opacity: 1 }}
+      // Fade out when the element leaves the viewport:
+      exit={{ opacity: 0 }}
+      // Animate the component when its layout changes:
+      layout
     >
-      <div className="card-body flex flex-col items-center justify-center h-full relative transition-all duration-500">
-        <h5
-          className="card-title mb-2.5 opacity-80 group-hover:translate-y-[-10px] transition-transform duration-500"
-          style={{ color: textColor }}
-        >
-          {color}
-        </h5>
-        <p
-          className="opacity-80 group-hover:opacity-0 group-hover:translate-y-[-10px] transition-all duration-500"
-          style={{ color: textColor }}
-        >
-          {name}
-        </p>
-        <span
-          className={`${
-            isCopied ? "opacity-0" : "opacity-0 group-hover:opacity-80"
-          } group-hover:translate-y-[-20px] icon-[tabler--copy] text-base-content/80 size-6 transition-all duration-500 absolute bottom-8 cursor-pointer`}
-          onClick={handleCopyClick}
-          style={{ color: textColor }}
-        ></span>
-        {isCopied && (
-          <span
-            className="opacity-80 font-bold translate-y-[-20px] -rotate-6 text-base-content/80 absolute bottom-8 transition-opacity duration-500 animate-chibolita-exit"
+      <div
+        className="card sm:max-w-44 h-52 group relative overflow-hidden shadow-flat"
+        style={{ backgroundColor: color }}
+      >
+        <div className="card-body flex flex-col items-center justify-center h-full relative transition-all duration-500">
+          <h5
+            className="card-title mb-2.5 opacity-80 group-hover:translate-y-[-10px] transition-transform duration-500"
             style={{ color: textColor }}
           >
-            copied!
-          </span>
-        )}
+            {color}
+          </h5>
+          <p
+            className="opacity-80 group-hover:opacity-0 group-hover:translate-y-[-10px] transition-all duration-500"
+            style={{ color: textColor }}
+          >
+            {name}
+          </p>
+          <span
+            className={`${
+              isCopied ? "opacity-0" : "opacity-0 group-hover:opacity-80"
+            } group-hover:translate-y-[-20px] icon-[tabler--copy] text-base-content/80 size-6 transition-all duration-500 absolute bottom-8 cursor-pointer`}
+            onClick={handleCopyClick}
+            style={{ color: textColor }}
+          ></span>
+          {isCopied && (
+            <span
+              className="opacity-80 font-bold translate-y-[-20px] -rotate-6 text-base-content/80 absolute bottom-8 transition-opacity duration-500 animate-chibolita-exit"
+              style={{ color: textColor }}
+            >
+              copied!
+            </span>
+          )}
+        </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
