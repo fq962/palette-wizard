1f53e483df522bbddf175d08b79606ba3d7c2173
Milton Barrientos <miltonbarrientos2@gmail.com>
2024-11-26 18:50:49 -0600

    corrección de versión de GPT

--- src/components/CreditsAndLinks.tsx
+++ src/components/CreditsAndLinks.tsx
@@ -1,7 +1,7 @@
 export default function CreditsAndLinks() {
   return (
     <small className="flex font-sf-display gap-2 opacity-75">
-      <a href="https://openai.com/">Powered by OpenAI&apos;s GPT-4</a>
+      <a href="https://openai.com/">Powered by OpenAI&apos;s GPT-4o</a>
       <span>-</span>
       <a href="https://creativecommons.org/licenses/by/4.0/">
         Creative Commons
