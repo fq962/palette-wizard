7d17f1976dcaf0b0b0f8d2615c3ffe0f6decc104
haide <df.quintanillah@gmail.com>
2024-12-04 08:36:09 -0600

    feat: implement dynamic version display in GitHubRepo component

--- src/components/GitHubRepo.tsx
+++ src/components/GitHubRepo.tsx
@@ -1,6 +1,9 @@
+import { getVersion } from "@/utils/get-pkg-version";
+
 export default function GitHubRepo(
   { className }: { className?: string } = { className: "" }
 ) {
+  const version = getVersion();
   return (
     <a
       href="https://github.com/fq962/palette-wizard"
@@ -8,7 +11,7 @@ export default function GitHubRepo(
       className={`flex items-center gap-2 font-sf-display hover:scale-105 p-1 rounded-md transition-all duration-150 ease-in-out cursor-pointer ${className}`}
     >
       <span className="icon-[tabler--brand-github]"></span>
-      <pre className="text-xs">v1.2</pre>
+      <pre className="text-xs">v{version}</pre>
     </a>
   );
 }
